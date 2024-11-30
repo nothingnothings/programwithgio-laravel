601af0f6051c4b90d67dc9481b756d54a70e2317
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2024-11-30 19:27:24 -0300

    finished lesson 10

--- relearning-laravel/gio-example/routes/web.php
+++ relearning-laravel/gio-example/routes/web.php
@@ -2,6 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return 'Welcome to the dashboard';
+});
+
+Route::get('/users', function () {
+    return ['John', 'Mary', 'Peter'];
+});
+
+Route::redirect('/home', '/dashboard');
